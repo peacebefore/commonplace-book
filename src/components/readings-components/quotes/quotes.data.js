8bcd7a4be536411ca7db298e9f2b6a95383b4c33
Priya <42794888+peacebefore@users.noreply.github.com>
2021-03-05 16:36:04 -0800

    added quote generator to readings page

---
+++ src/components/readings-components/quotes/quotes.data.js
@@ -0,0 +1,40 @@
+const QUOTE_DATA = [
+    {
+      id: "1",
+      text: "I-I am going to be a storm-a flame-\nI need to fight whole armies alone;\nI have ten hearts; I have a hundred arms;\nI feel too strong to war with mortals-\nBRING ME GIANTS!",
+      author: "Edmund Rostand",
+      source: "Cyrano de Bergerac"
+    },
+    {
+      id: "2",
+      text: "But I, being poor, have only my dreams;\nI have spread my dreams under your feet;\nTread softly because you tread on my dreams.",
+      author: "W.B. Yeats",
+      source: "He Wishes for the Cloths of Heaven"
+    },
+    {
+      id: "3",
+      text: "Let us go then, you and I,\nWhen the evening is spread out against the sky",
+      author: "T.S. Eliot",
+      source: "The Love Song of J. Alfred Prufrock"
+    },
+    {
+      id: "4",
+      text: "The yellow fog that rubs its back upon the window-panes,\nThe yellow smoke that rubs its muzzle on the window-panes,\nLicked its tongue into the corners of the evening,\nLingered upon the pools that stand in drains,\nLet fall upon its back the soot that falls from chimneys,\nSlipped by the terrace, made a sudden leap,\nAnd seeing that it was a soft October night,\nCurled once about the house, and fell asleep.",
+      author: "T.S. Eliot",
+      source: "The Love Song of J. Alfred Prufrock"
+    },
+    {
+      id: "5",
+      text: "i carry your heart with me(i carry it in\nmy heart)i am never without it(anywhere\ni go you go,my dear;and whatever is done\nby only me is your doing,my darling)\n\t\t\t\t\ti fear\nno fate(for you are my fate,my sweet)i want\nno world(for beautiful you are my world,my true)\nand it’s you are whatever a moon has always meant\nand whatever a sun will always sing is you",
+      author: "ee cummings",
+      source: "i carry your heart(i carry it in my heart)"
+    },
+    {
+      id: "6",
+      text: "I have eaten\nthe plums\nthat were in\nthe icebox\rand which\nyou were probably\nsaving\nfor breakfast\rForgive me\nthey were delicious\nso sweet\nand so cold",
+     author: "William Carlos Williams",
+      source: "This is Just to Say"
+    }
+  ]
+
+export default QUOTE_DATA;
--- src/pages/readings/readings.component.jsx
+++ src/pages/readings/readings.component.jsx
@@ -1,11 +1,12 @@
 // includes quotes, books I've read, things I want to read or watch, things I want to keep for reference (resources)
 
+import QuoteApp from '../../components/readings-components/quotes/containers/app';
 import './readings.styles.scss';
 
 const Readings = () => {
   return (
     <div className='readings'>
-      <h1>Readings</h1>
+      <QuoteApp />
     </div>
   );
 };
